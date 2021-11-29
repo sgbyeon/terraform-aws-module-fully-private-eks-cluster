c7c2a4eeda2aa579cfa5c193375c24c0317a293c
sgbyeon <sg.byeon1@gmail.com>
2021-11-29 18:12:24 +0900

    [bsg] first commit

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "account_id" {
+  description = "List of Allowed AWS account IDs"
+  type = list(string)
+  default = [""]
+}
+
+variable "region" {
+  description = "AWS Region"
+  type = string
+  default = ""
+}
+
 variable "prefix" {
   description = "prefix for aws resources and tags"
   type = string
