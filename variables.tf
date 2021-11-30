b7294ad33077687360d3ea3519db36ac5b44c6b1
sgbyeon <sg.byeon1@gmail.com>
2021-11-30 15:25:47 +0900

    [bsg] update variables.tf

--- variables.tf
+++ variables.tf
@@ -71,7 +71,7 @@ variable "private_subnet_ids" {
 
 variable "private_subnet_cidr" {
   description = "private subnet cidr block"
-  type = list(string)
+  type = string
 }
 
 variable "private_route_tables" {
