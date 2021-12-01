3ea148b8a1f64b6a6ad7335193298046fda9e352
sgbyeon <sg.byeon1@gmail.com>
2021-12-01 17:09:54 +0900

    [bsg] Added var.bastion_ipv4_cidr to cidr_block in aws_security_group_rule resource in securitygroup.tf file.

--- securitygroup.tf
+++ securitygroup.tf
@@ -57,6 +57,6 @@ resource "aws_security_group_rule" "vpce" {
   from_port = 443
   to_port = 443
   protocol = "tcp"
-  cidr_blocks = [ "${var.private_subnet_cidr}", "${var.bastion_ipv4_cidr}" ]
+  cidr_blocks = concat(var.private_subnet_cidr, var.bastion_ipv4_cidr)
   security_group_id = aws_security_group.vpce.id
 }
